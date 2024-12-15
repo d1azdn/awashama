11db3c9948def0771c404091d0235279a2c8124c
d1azdn <diazsaputramc@gmail.com>
2024-12-15 16:57:20 +0700

    fixed user checkout->processing status

--- src/components/ShopNavbar.jsx
+++ src/components/ShopNavbar.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
 export function ListNavbar(props){
     return(
         <>
@@ -10,6 +13,37 @@ export function ListNavbar(props){
 }
 
 export default function ShopNavbar(){
+    const [userInfo, setUserInfo] = useState([])
+    const [loginPopup, setLoginPopup] = useState(false)
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+            const cekLogin = async () =>{
+                const response = await fetch(import.meta.env.VITE_API_URL + '/cekrole',{
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    credentials : "include"
+                })
+                setUserInfo(await response.json())
+            }
+    
+            cekLogin()
+        },[])
+    
+        const handleLogout = async (e)=>{
+            const response = await fetch(import.meta.env.VITE_API_URL + '/logout', {
+                method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                credentials : "include"
+            })
+            setLoginPopup(!loginPopup)
+            setUserInfo({role:'guest'})
+            navigate("/")
+        }
     return(
         <>
         <nav className="flex justify-between py-3 px-16 items-center shadow-md">
@@ -32,16 +66,35 @@ export default function ShopNavbar(){
             </li>
 
             <li className="hover:scale-125 duration-200">
-                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z"/>
-                </svg>
+            <a href="/keranjang/proses">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M7 8H12V10H7V8Z" fill="black"/><path d="M7 12H17V14H7V12Z" fill="black"/><path fillRule="evenodd" clipRule="evenodd" d="M9 1V2H11V1H13V2H15V1H17V2H21V23H3V2H7V1H9ZM7 4H5V21H19V4H17V5H15V4H13V5H11V4H9V5H7V4Z" fill="black"/></svg>
+            </a>
             </li>
 
-            <li className="hover:scale-125 duration-200">
+            <li className="hover:scale-105 duration-200 flex gap-2 items-center justify-end hover:cursor-pointer">
                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
             </svg>
-
+            { userInfo.role == "admin" ? 
+            (<>
+                <p onClick={()=>setLoginPopup(!loginPopup)}>Halo, {userInfo.username}!</p>
+                <div className={`mt-32 border border-solid fixed flex flex-col shadow-md ${loginPopup?'':'hidden'}`}>
+                    <button className="bg-awashama-white hover:bg-awashama-lightgray ps-14 pe-6 py-3" onClick={()=>navigate("/dashboard/produk")}>Dashboard</button>
+                    <button className="bg-awashama-white hover:bg-awashama-lightgray ps-14 pe-6 py-3" onClick={()=>handleLogout()}>Logout</button>
+                </div>
+            </>)       
+            : 
+            userInfo.role == "user" ? 
+            (<>
+                <p onClick={()=>setLoginPopup(!loginPopup)}>Halo, {userInfo.username}!</p>
+                <div className={`mt-20 border border-solid fixed flex flex-col shadow-md ${loginPopup?'':'hidden'}`}>
+                    <button className="bg-awashama-white hover:bg-awashama-lightgray ps-14 pe-6 py-3" onClick={()=>handleLogout()}>Logout</button>
+                </div>
+            </>) 
+            : 
+            (
+                <p onClick={()=>navigate('/login')}>Login</p>
+            )}    
             </li>
 
             </ul>
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -4,14 +4,46 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function ProductList(props){
-    const total = props.jumlah * props.hargaSatuan
+    
+    const { setTotalPrice } = props
+    const { getKeranjang } = props
+    const total = props.jumlah * props.harga
+    let runOnce = false
+
+    const deleteConfirmation = async (id)=>{
+        if (window.confirm("Are you want to delete with id : "+id)){
+            const response = await fetch(import.meta.env.VITE_API_URL + '/checkout/'+id,{
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                credentials : "include"
+            })
+        } getKeranjang()
+    }
+
+    useEffect(()=>{
+        if (total){
+            if (!runOnce){
+                setTotalPrice(prev=>(isNaN(prev) ? 0 : prev)+total)
+                runOnce = true
+            }
+        }
+    },[total])
+    
     return(
         <>
-        <div className="cart grid grid-cols-5">
-            <h1>Centang</h1>
-            {/* <img src={props.imgUrl} alt="..." width={150}/> */}
-            <h1>{props.productId}</h1>
-            <h1>Rp. {props.hargaSatuan}</h1>
+        <div className="cart grid grid-cols-5 items-center">
+            <div className="">
+            <p className="bg-awashama-red p-2 mx-10 rounded-xl h-10 hover:bg-awashama-white hover:text-awashama-black text-awashama-white hover:cursor-pointer" onClick={()=>{deleteConfirmation(props.id_cart)}}>Hapus</p>
+            <p>cart id : {props.id_cart}</p>
+            </div>
+            <div className="img">
+                <img src={props.foto} alt="..." width={125}/>
+                <h1>id: {props.productId}</h1>
+                <h1 className='break-words font-semibold text-lg'>{props.produk} </h1>
+            </div>
+            <h1>Rp. {props.harga}</h1>
             <h1>{props.jumlah}</h1>
             <h1>Rp. {total}</h1>
         </div>
@@ -19,29 +51,137 @@ function ProductList(props){
     )
 }
 
+
+
+function ProductSum(props){
+    const total = props.jumlah * props.harga
+    return(
+        <>
+        <div className="grid grid-cols-3">
+            <h1>{props.produk}</h1>
+            <h1>{props.jumlah} buah</h1>
+            <h1>Rp. {total}</h1>
+        </div>
+        </>
+    )
+}
+
+
+
 export default function Cart(){
     const [cart, setCart] = useState([]);
+    const [userInfo, setUserInfo] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const [productInfo, setProductInfo] = useState([])
     const [totalPrice, setTotalPrice] = useState(0);
+
+    const [moreFormPopup, setMoreFormPopup] = useState(false)
+    
     const navigate = useNavigate();
 
-    useEffect(()=>{
-        fetch("/keranjangTest.json")
-        .then(response => response.json())
-        .then(data=>setCart(data))
-        .catch((error) => console.error("Error fetching articles:", error))
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        const form = e.target;
+        const formData = new FormData(form);
+        
+        const formDataJSON = {};
+        formData.forEach((value, key) => {
+            formDataJSON[key] = value;
+        });
+        console.log(formDataJSON)
 
-        const total = cart.reduce((sum, item) => sum + parseInt(item.harga*item.jumlah), 0);
-        setTotalPrice(total);
-    })
+        const response = await fetch(import.meta.env.VITE_API_URL+'/checkout/proses', {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            mode:'cors',
+            body: JSON.stringify(formDataJSON),
+            credentials : "include"
+          })
+          if (!response.ok) {
+            const errorMessage = await response.text();
+            setErrorMessage(errorMessage);
+          } else{
+            navigate("/keranjang/proses")
+          }
+    }
+
+    const checkCartNotNull = ()=>{
+        if (cart != ''){
+            setMoreFormPopup(!moreFormPopup)
+        }
+    }
+
+    const getKeranjang = async () =>{
+        try{
+            const response = await fetch(import.meta.env.VITE_API_URL + '/checkout/'+userInfo.id,{
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                credentials : "include"
+            })
+            const data = await response.json()
+            setCart(data.filter(item=>item.status == "keranjang"))
+        }catch(error){
+            setCart('')
+        }
+        
+    }
+
+    useEffect(()=>{
+        if (userInfo!=''){
+            const getProduk = async () =>{
+                const response = await fetch(import.meta.env.VITE_API_URL + '/produk',{
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    credentials : "include"
+                })
+                const data = await response.json()
+                setProductInfo(data)
+            }
+
+            getProduk()
+            
+            getKeranjang()
+
+        }
+    },[userInfo])
+
+    useEffect(() => {
+        const cekUser = async () =>{
+            const response = await fetch(import.meta.env.VITE_API_URL + '/cekrole',{
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                credentials : "include"
+            })
+            const checkAdmin = await response.json()
+            if (checkAdmin?.role == 'guest'){
+                navigate('/login')
+                return
+            }
+            setUserInfo(checkAdmin)
+        }
+        cekUser()
+      }, []);
 
     return(
         <>
-        <div className="beranda mx-32 mt-5 bg-awashama-toolightgray shadow-md w-48 rounded-xl hover:bg-awashama-lightgray duration-200 hover:cursor-pointer">
+        <form action="#" method='PUT' onSubmit={handleSubmit} id='myForm'>
+            <input type="hidden" name="id_user" id='id_user' defaultValue={userInfo.id}/>
+            <input type="hidden" name="status" id='status' defaultValue={"proses"}/>
+        <div className={`beranda mx-32 mt-5 bg-awashama-toolightgray shadow-md w-48 rounded-xl hover:bg-awashama-lightgray duration-200 hover:cursor-pointer ${moreFormPopup?'hidden':''}`}>
             <p className='p-3 font-semibold' onClick={()=>{navigate(-1)}}>Kembali</p>
         </div>
 
-        <section className='keranjang grid grid-cols-4 gap-5 mx-32 mt-2 mb-8'>
-            <div className="item grid grid-cols-1 col-span-3 p-8 bg-awashama-toolightgray rounded-xl h-fit">
+        <section className={`keranjang grid grid-cols-4 gap-5 mx-32 mt-2 mb-8 ${moreFormPopup?'hidden':''}`}>
+            <div className="item grid grid-cols-1 col-span-3 p-8 bg-awashama-toolightgray rounded-xl h-fit gap-5">
                 <div className="info grid grid-cols-5 font-semibold">
                     <h1>-</h1>
                     <h1>Produk</h1>
@@ -50,12 +190,25 @@ export default function Cart(){
                     <h1>Subtotal</h1>
                 </div>
                 {
-                    cart.map((item)=>(
-                        <ProductList productId={item.id_produk} jumlah={item.jumlah_produk} hargaSatuan={item.jumlah_produk} key={item.productid}/>
-                    ))
+                    cart.length == 0 ? 
+                    (<p>Kosong.</p>)
+                    :
+                    (
+                        cart.map((item, index)=>(
+                            <ProductList 
+                            productId={item.id_produk} id_cart={item.id}
+                            foto={productInfo.find(element=>element.id == item.id_produk)?.foto} 
+                            produk={productInfo.find(element=>element.id == item.id_produk)?.nama_produk} 
+                            harga={productInfo.find(element=>element.id == item.id_produk)?.harga} 
+                            jumlah={item.jumlah_produk} hargaSatuan={item.jumlah_produk} key={index}
+                            setTotalPrice={setTotalPrice}
+                            getKeranjang={getKeranjang}/>
+                        ))  
+                    )
+                    
                 }
             </div>    
-
+            
             <div className="total bg-awashama-toolightgray rounded-xl p-4">
                 <div className="card grid grid-cols-2 mb-4">
                     <h1 className='font-semibold text-lg'>Total Belanja</h1>
@@ -67,22 +220,22 @@ export default function Cart(){
                 </div>
                 <label htmlFor="promo">Kode Promo</label>
                 <input type="text" name="promo" id='promo' className='font-semibold border-2 border-awashama-lightgreen p-3 text-awashama-black placeholder-awashama-lightgray rounded-xl w-full mt-2' placeholder='Masukkan kode promo' />
-                <input type="submit" value="Beli" className='mt-2 font-semibold bg-awashama-lightgreen p-3 w-full rounded-xl hover:cursor-pointer hover:bg-awashama-darkgreen' />
+                <p onClick={()=>checkCartNotNull()} className='mt-2 font-semibold bg-awashama-lightgreen p-3 w-full rounded-xl hover:cursor-pointer hover:bg-awashama-darkgreen text-center'>Beli</p>
             </div>
         </section>
 
-        <section className='tambahan grid grid-cols-2 mx-32 gap-5'>
+        <section className={`tambahan grid grid-cols-2 mx-32 gap-5 my-10 ${moreFormPopup?'':'hidden'}`}>
             <div className="detail flex flex-col">
                 <h1 className='font-semibold text-2xl mb-4'>Penagihan dan pengiriman</h1>
                 
                 <label htmlFor="nama" className='font-semibold text-lg mb-1'>Nama lengkap</label>
-                <input type="text" name="nama" id="nama" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan nama anda.'/>
-                <label htmlFor="Alamat" className='font-semibold text-lg mb-1'>Alamat</label>
-                <input type="text" name="Alamat" id="Alamat" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan alamat anda.'/>
-                <label htmlFor="Telepon" className='font-semibold text-lg mb-1'>Nomor telepon</label>
-                <input type="text" name="Telepon" id="Telepon" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan nomor telepon anda.'/>
-                <label htmlFor="catatan" className='font-semibold text-lg mb-1'>Catatan (Opsional)</label>
-                <input type="text" name="catatan" id="catatan" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Tulis catatan disini.'/>
+                <input type="text" name="nama" id="nama" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan nama anda.' required/>
+                <label htmlFor="alamat" className='font-semibold text-lg mb-1'>Alamat</label>
+                <input type="text" name="alamat" id="alamat" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan alamat anda.' required/>
+                <label htmlFor="telepon" className='font-semibold text-lg mb-1'>Nomor telepon</label>
+                <input type="text" name="telepon" id="telepon" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Masukkan nomor telepon anda.' required/>
+                <label htmlFor="note_pelanggan" className='font-semibold text-lg mb-1'>Catatan (Opsional)</label>
+                <textarea type="text" name="note_pelanggan" id="note_pelanggan" className='text-lg p-2 border-2 border-awashama-lightgray rounded-xl mb-4' placeholder='Tulis catatan disini.' rows={10} cols={10}/>
 
             </div>
             <div className="konfirmasi">
@@ -93,32 +246,39 @@ export default function Cart(){
                         <h1>Jumlah</h1>
                         <h1>Sub total</h1>
                     </div>
-                    <div className="grid grid-cols-3">
-                        <h1>Nama produk</h1>
-                        <h1>2 buah</h1>
-                        <h1>Rp. 2000</h1>
-                    </div>
+                    {
+                        cart.length == 0 ? 
+                        (<p>kosong</p>)
+                        :
+                        (cart.map((item,index)=>(
+                            <ProductSum key={index} jumlah={item.jumlah_produk} 
+                            produk={productInfo.find(element=>element.id == item.id_produk)?.nama_produk} 
+                            harga={productInfo.find(element=>element.id == item.id_produk)?.harga}/>
+                        )))
+                    }
+                    
 
                     <div className="splitter border border-solid mt-8 mb-2"></div>
 
                     <div className="grid grid-cols-2 mb-4 font-semibold text-lg">
                         <h1>Total harga</h1>
-                        <h1>Rp. 2000</h1>
+                        <h1>Rp. {totalPrice}</h1>
                     </div>
 
                     <div className="pembayaran flex flex-col">
                         <h1 className='font-semibold text-xl mb-1'>Pembayaran</h1>
-                        <select name="pembayaran" id="pembayaran" className='w-1/2 rounded-xl p-2 mb-4'>
+                        <select name="metode_pembayaran" id="metode_pembayaran" className='w-1/2 rounded-xl p-2 mb-4' required>
                             <option value="gopay">Gopay</option>
                             <option value="dana">Dana</option>
                         </select>
                     </div>
 
-                    <input type="submit" value="Bayar sekarang" className='bg-awashama-lightgreen rounded-xl p-4 font-semibold w-full'/>
+                    <input type="submit" value="Bayar sekarang" form="myForm" className='bg-awashama-lightgreen hover:text-awashama-darkgreen hover:bg-awashama-toolightgreen rounded-xl p-4 font-semibold w-full hover:cursor-pointer duration-200'/>
+                    <p onClick={()=>checkCartNotNull()} className='mt-2 font-semibold border-2 border-solid border-awashama-red hover:bg-awashama-red bg-awashama-white hover:text-awashama-white p-3 w-full rounded-xl hover:cursor-pointer text-center duration-200'>Batal</p>
                 </div>
             </div>
         </section>
-
+        </form>
         </>
     )
 }
